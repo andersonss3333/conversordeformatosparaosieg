adc5a390fbda733eea2ff88b05a29497ef0fb62c
AndersonSanto54 <17697502-AndersonSanto54@users.noreply.replit.com>
2023-08-21 01:03:16 +0000

    Criado a classe de testes unitários: LerArquivosTest
    Criada a classe de testes unitários para leitura de arquivos.

---
+++ tests/Unit/LerArquivosTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class LerArquivosTest extends TestCase
+{
+   use WithFaker;
+  
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_limparCgaNaoRetornaNada()
+    {
+        $this->assertTrue(true);
+    }
+}
