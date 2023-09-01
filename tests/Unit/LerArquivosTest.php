311a6c8b1ee810834da159d60a525376c1b24e04
AndersonSanto54 <17697502-AndersonSanto54@users.noreply.replit.com>
2023-09-01 00:59:18 +0000

    Ampliado o arquivo de testes
    Acresntado os testes para o arquivo com mais de 200 linhas.
    Criado um segundo arquivo arquivo2.txt

--- tests/Unit/LerArquivosTest.php
+++ tests/Unit/LerArquivosTest.php
@@ -10,6 +10,7 @@ class LerArquivosTest extends TestCase
 {
 
   private $dados= null;
+  private $dados2= null;
   
   public function setUp(): void
   {
@@ -29,6 +30,7 @@ class LerArquivosTest extends TestCase
     fclose($arquivoTexto);
     
     $this->dados= new LerArquivosTextoSalvador('arquivo.txt');
+    $this->dados2= new LerArquivosTextoSalvador('arquivo2.txt');
     
   }
   
@@ -57,6 +59,73 @@ class LerArquivosTest extends TestCase
         $this->assertMatchesRegularExpression('/[0-9]{2},[0-9]{3},[0-9]{3}\/[0-9]{4}-[0-9]{2}|[0-9]{11}/', $cnpjCga);
       }
   }
+
+  public function dadosArquivoTeste(): array
+  {
+    $faker= \Faker\Factory::create('pt_BR');
+    
+    $dadosParaTeste= "{$faker->text($maxNbChars= 35)}\n";
+    
+    $linhas= "{$faker->numerify('#')} {$faker->date('Y-m-d', $max= 'now')} {$faker->numerify('###.###/###-##')} {$faker->numerify('##.##')} {$faker->numerify('##.###.###/####-##')}\n
+        {$faker->numerify('#')} {$faker->date('Y-m-d', $max= 'now')} {$faker->numerify('###.###/###-##')} {$faker->numerify('##.##')} {$faker->numerify('##.###.###/####-##')}\n
+        {$faker->numerify('#')} {$faker->date('Y-m-d', $max= 'now')} {$faker->numerify('###.###/###-##')} {$faker->numerify('##.##')} {$faker->numerify('##.###.###/####-##')}\n";
+
+    $numero= 0;
+
+    for($numero; $numero <= 39; ++$numero)
+      {
+        $dadosParaTeste .= $linhas;
+      }
+      
+    return [[$dadosParaTeste]];
+  }
+
+  /*-_------------------|-----------------------*/
+  /*
+  * Testando arwuivo com 200 linhas
+  */
+
+  /**
+  *
+  *
+  * @dataProvider dadosArquivoTeste
+  */
+  public function test_gravarArquivoCom200Linhas(string $texto)
+  {
+    $arquivo= fopen('arquivo2.txt', 'w');
+
+    fwrite($arquivo, $texto);
+    fclose($arquivo);
+    
+    $this->assertCount(201, file('arquivo2.txt'));
+  }
+
+      public function test_processarArquivos200LinhasRetornaUmArray()
+    { 
+      $this->assertIsArray($this->dados2->processarArquivos());
+      
+    }
+
+    public function test_processarArquivos200LinhasArrayNaoEstarVazio()
+  {
+    $this->assertNotEmpty($this->dados2->processarArquivos());
+  }
+
+  public function test_processarArquivos200LinhasPossuiSomenteStrings()
+  {
+    $this->assertContainsOnly('string', $this->dados2->processarArquivos());
+  }
+
+  public function test_processarArquivos200LinhssRetornaOsDadosNoFormatoCorreto()
+  {
+    $dados= $this->dados2->processarArquivos();
+    
+    foreach($dados as $cnpjCga)
+      {
+        $this->assertMatchesRegularExpression('/[0-9]{2},[0-9]{3},[0-9]{3}\/[0-9]{4}-[0-9]{2}|[0-9]{11}/', $cnpjCga);
+      }
+  }
+
   
   public function tearDown(): void
   {
