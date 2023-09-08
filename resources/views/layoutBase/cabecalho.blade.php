2c3dad46be9215bb5dcdf9575af4b6dfc07ab8de
andersonss3333 <61986093+andersonss3333@users.noreply.github.com>
2023-09-08 00:09:00 +0000

    Mudança no cabeçalho
    Adionado a TAG de abertura HTML.
    Adicionada as tags script e link, com a chamada aos respectivos métodos mix.

--- resources/views/layoutBase/cabecalho.blade.php
+++ resources/views/layoutBase/cabecalho.blade.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+
+    <html>
+
     <head>
         <title> Conversor Formatação para o Domínio </title>
 
@@ -7,5 +10,9 @@
         <meta name='viewport' content='width= device-width, initial-scale= 1' />
         <meta http-equiv='X-UA-Compatible' content='ie-edge' />
         <meta name='csrf_token' content='{{ csrf_token() }}' />
+
+        <script src="{{ mix('/js/app.js') }}" type='text/javascript' defer></script>
+
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet" type='text/css' />
 
     </head>
