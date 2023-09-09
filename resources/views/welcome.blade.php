82bb60488e0e6125d11e462e425fb0cceb46902e
andersonss3333 <61986093+andersonss3333@users.noreply.github.com>
2023-09-09 16:47:08 +0000

    Modificada página principal.
    Adicionado na label seminbold e aumentado o tamanho da fonte.
    Adicionado bold medium e itálico a mensagem de aviso do input file.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,18 +11,18 @@
         @csrf
 
         <div class='row mb-3'>
-           <label for='arquivosalvador' class='col-sm-2 col-form-label'> Converta seu arquivo texto de Salvador </label>
+           <label for='arquivosalvador' class='col-sm-2 col-form-label fw-semibold fs-4'> Converta seu arquivo texto de Salvador </label>
         <div class='col-sm-6'>
            <input name='arquivosalvador' type='file' id='arquivosalvador' class='form-control form-control-sm' aria-describedby='aviso' />
 @error('arquivosalvador') 
            <span class='text-danger'> {{ $message }} </span>
 @enderror
-           <div id='aviso' class='form-text'>Somente arquivo do tipo .txt </div>
+           <div id='aviso' class='form-text fs-6 fst-italic fw-medium'>Somente arquivo do tipo .txt </div>
         </div>
         </div>
         </div>
 
-           <button class='btn btn-sm' type='submit' id='arquivotextobutton' >Converter Arquivo </button>
+           <button class='btn btn-primary btn-sm' type='submit' id='arquivotextobutton' >Converter Arquivo </button>
 
         </form>
 
