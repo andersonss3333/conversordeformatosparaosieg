275c46c103acff87bf61891286652d5a71ecd593
Anderson Souza <andersondevcompleto@gmail.com>
2023-09-14 21:43:22 +0000

    Remida chamada para a classe Schedule e excluído o método schedule.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,20 +2,10 @@
 
 namespace App\Console;
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
-    }
 
     /**
      * Register the commands for the application.
